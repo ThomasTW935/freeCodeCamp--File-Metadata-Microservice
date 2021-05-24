b1b3600affac82f43cb0268a6e9b8a354871750a
ThomasTW935 <thomastw935@gmail.com>
2021-05-24 09:19:38 +0800

    Initail Commit

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer')
 require('dotenv').config()
 
-var app = express();
 
+const upload = multer({dest: 'uploads/'})
+var app = express();
+app.use(express.urlencoded({extended: true}))
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -11,10 +14,18 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req,res)=>{
+  console.log(req.file)
+  const file = req.file
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  })
+})
 
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port http://localhost:' + port)
 });
